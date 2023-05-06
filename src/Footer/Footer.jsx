7f21afbf99e76fa90d84c6a6e2266c2b9563f16c
tush27 <tusharsaini1301@gmail.com>
2023-05-06 13:56:06 +0530

    First commit

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     <a href="https://www.linkedin.com/in/tush27/"><img src="./images/linkedin-logo.png" alt="linkedIn" className="social-img"/></a>
     <a href="https://www.instagram.com/tushar.1301/"><img src="/images/instagram-logo.png" alt="instagram"
         className="social-img"/></a>
-        <hr style={{backgroundColor: "white", border: "none",height: "1px",opacity:"1",margin:"5px 70px"}}/>
+        <hr style={{backgroundColor: "white", border: "none",height: "1px",opacity:"1",margin:"5px"}}/>
     Made with <b>{ch}</b> by <i>Tushar</i>
 </footer>
   )
