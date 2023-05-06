32d064f8f74e57af8e0b24ac2b57e2cc6e3ac896
tush27 <tusharsaini1301@gmail.com>
2023-05-06 18:04:27 +0530

    an update

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer>
     <a href="https://github.com/tush27"><img src="./images/github-logo.png" alt="github" className="social-img"/></a>
     <a href="https://www.linkedin.com/in/tush27/"><img src="./images/linkedin-logo.png" alt="linkedIn" className="social-img"/></a>
-    <a href="https://www.instagram.com/tushar.1301/"><img src="/images/instagram-logo.png" alt="instagram"
+    <a href="https://www.instagram.com/tushar.1301/"><img src="./images/instagram-logo.png" alt="instagram"
         className="social-img"/></a>
         <hr style={{backgroundColor: "white", border: "none",height: "1px",opacity:"1",margin:"5px"}}/>
     Made with <b>{ch}</b> by <i>Tushar</i>
